2999da1159245d16efcc7f4fc543e4bfc205238c
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-01-26 21:39:18 -0700

    added readme, updated connection, controller/index, and public js files

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+let sequelize;
+
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -10,7 +17,7 @@ const sequelize = new Sequelize(
       dialect: 'mysql',
       port: 3306,
     }
-  );
+  )};
   
   module.exports = sequelize;
   
