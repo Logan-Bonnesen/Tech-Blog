ab15c71ee6fe185ccddaf706ade0ba6981676771
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-02-06 21:08:51 -0700

    updating main.handlebars

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,14 +1,14 @@
 const commentHandler = async function (event) {
     event.preventDefault();
 
-    const post = document.querySelector('input[name="post"]').value;
+    const post = document.querySelector('input[name="post-id"]').value;
     const comment_text = document.querySelector('textarea[name="comment-body"]')
 
     if (comment_text) {
         await fetch('/api/comment', {
           method: 'POST',
           comment_text: JSON.stringify({
-            postId,
+            post,
             comment_text
           }),
           headers: {
